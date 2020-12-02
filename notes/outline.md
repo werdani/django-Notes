f364b38d9ab5c19202f8fd082993aab3167225ce
werdani <https://github.com/werdani/django-job-board.git>
2020-12-02 19:23:14 +0200

    create models for note app

---
+++ notes/outline.md
@@ -0,0 +1,9 @@
+create models for nots app :
+  -title
+  -contant
+  -date and time
+  -tages
+  -active
+  -slug
+  -user
+  
--- notes_app/models.py
+++ notes_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime
+from django.utils.text import slugify
 # Create your models here.
+
+
+class Note(models.Model):
+    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+    title   = models.CharField(max_length=100)
+    slug    = models.SlugField(blank=True, null=True)
+    content = models.TextField(blank=True)
+    created = models.DateTimeField(blank= True, default =datetime.datetime.now)
+    active  = models.BooleanField(default=True)
+    tags    = models.CharField(blank = True, max_length=100)
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug =slugify(self.title)
+        super(Note,self).save(*args,**kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Note'
+        verbose_name_plural = 'Notes' 
+
