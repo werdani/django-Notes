b7400144bf62740d019ded6ad61609ac391bbf0a
werdani <https://github.com/werdani/django-job-board.git>
2020-12-09 13:18:25 +0200

    add views

--- notes_app/views.py
+++ notes_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Note
 
 # Create your views here.
 
 def all_notes(request):
-    return HttpResponse('hello ammar')
+    #return HttpResponse('hello ammar')
+    all_notes = Note.objects.all()
+
+    return render(request,'all_notes.html',{'all_notes':all_notes})
+
 
 
 def detail(request):
-    return HttpResponse('<h1> hello werdani </h1>')
+    #return HttpResponse('<h1> hello werdani </h1>')
+    pass
