c857916e95cff178d36d3fe63aac33cd6fdc6d63
werdani <https://github.com/werdani/django-job-board.git>
2020-12-20 17:59:00 +0200

    edit admin profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import Profile
 # Register your models here.
-admin.site.register(Profile)
+
+class ProfileAdinm(admin.ModelAdmin):
+    list_display = ['user','slug','headline','join']
+    list_filter = ['headline','join']
+    search_fields =['user__first_name'] # because user is forn key  and relation 
+    list_editable = ['slug']
+
+
+
+
+
+
+
+#user
+#slug
+#headline
+#bio
+#join
+admin.site.register(Profile,ProfileAdinm)
